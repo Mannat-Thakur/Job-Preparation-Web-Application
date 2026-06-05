7511fa864da4eb8f6e3354e6edc900e8a6bd5c4e
Mannat-Thakur <mannatthakur2512@gmail.com>
2026-06-06 02:39:43 +0530

    add user auth with register and login

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const authRouter = express.Router();
+
+
+
+const authController = require("../controller/auth.controller.js");
+/**   
+ * @route POST /api/auth/register
+ * @desc Register a new user
+ * @access Public
+ */   
+
+authRouter.post("/register", authController.registerUserController);
+
+/**
+ * @route POST /api/auth/login
+ * @desc Login user with email and password
+ * @access Public
+ */
+
+module.exports = authRouter;
+
