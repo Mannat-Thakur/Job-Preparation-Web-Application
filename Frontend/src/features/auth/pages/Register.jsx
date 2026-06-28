193d9390355bfe1b6d5a5a272caeb8ccfcbb100c
Mannat-Thakur <mannatthakur2512@gmail.com>
2026-06-28 23:01:01 +0530

    CORS Setupt, Procected Routes integrations

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import {useNavigate, Link} from 'react-router'
+import {useAuth} from "../hooks/useAuth.js";
 
 
 const Register = () => {
 
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const {loading, handleRegister} = useAuth();
+
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    
+    await handleRegister({username, email, password});
+    navigate("/");
   };
+
+  if(loading){
+    return (<main><h1>Loading.......</h1></main>)
+  }
 
   return (
     <main>
@@ -19,6 +30,7 @@ const Register = () => {
         <div className="input-group">
           <label htmlFor="username">Username</label>
           <input
+          onChange={(e)=> {setUsername(e.target.value)}}
             type="text"
             id="username"
             name="username"
@@ -30,6 +42,7 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input
+              onChange={(e)=> {setEmail(e.target.value)}}
               type="email"
               id="email"
               name="email"
@@ -40,6 +53,7 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
+              onChange={(e)=> {setPassword(e.target.value)}}  
               type="password"
               id="password"
               name="password"
